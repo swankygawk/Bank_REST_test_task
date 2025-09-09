cad54f7a1ddaf2ea2363b9cced09ec930c7ffbf0
Andrey Nosov <107930557+swankygawk@users.noreply.github.com>
2025-09-09 23:32:37 +0200

    Created User and Role entities, removed jjwt-bom to resolve jackson version conflict

---
+++ src/main/java/com/example/bankcards/entity/User.java
@@ -0,0 +1,53 @@
+package com.example.bankcards.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(
+    name = "users"
+)
+public class User {
+    @Id
+    @GeneratedValue(
+        strategy = GenerationType.UUID
+    )
+    @Column(
+        name = "id",
+        nullable = false,
+        updatable = false
+    )
+    private UUID id;
+
+    @Column(
+        name = "username",
+        unique = true,
+        nullable = false
+    )
+    private String username;
+
+    @Column(
+        name = "password",
+        nullable = false
+    )
+    private String password;
+
+    @ManyToMany(
+        fetch = FetchType.EAGER
+    )
+    @JoinTable(
+        name = "users_roles",
+        joinColumns = @JoinColumn(name = "user_id"),
+        inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles = new HashSet<>();
+}
